59dbdb02a2af49fbf56752d6d7f778593426fd9d
Aaditya Kumar <aaditya24643@gmail.com>
2026-08-30 20:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.22%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int c=0;
+      while(n>=1){
+        if(n%2==1){
+            c++;
+            n=n/2;
+        }
+        else if(n%2==0){
+            n=n/2;
+        }
+       
+       
+      }
+      return c;
+
+        
+    }
+};
